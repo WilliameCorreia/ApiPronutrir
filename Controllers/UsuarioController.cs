7939a92462968aaf79ae8a30069016aa4f5d51fb
williame <williame_lima@hotmail.com>
2020-07-14 14:09:38 -0300

    controllerUsuarios validações

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,3 +1,4 @@
+using ApiPronutrir.Models;
 using ApiPronutrir.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,16 +8,63 @@ namespace ApiPronutrir.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        private readonly MedicamentoService _medicamentoService;
+        private readonly UsuarioService _usuarioService;
 
-        public UsuarioController(MedicamentoService medicamentoService)
+        public UsuarioController(UsuarioService usuarioService)
         {
-            _medicamentoService = medicamentoService;
+            _usuarioService = usuarioService;
         }
 
-        public ActionResult Index(){
+        [HttpGet]
+        public ActionResult GetAll(){
 
-            //var Lista_medicamentos =
+            var Lista_usuarios = _usuarioService.GetAllUsuarios();
+
+            return Ok(Lista_usuarios);
         }
+
+        [HttpGet("{id}", Name="getUsuario")]
+        public ActionResult GetById(int id){
+
+            var usuario = _usuarioService.find(id);
+
+            if(!ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                return new ObjectResult(usuario);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] Usuarios usuario){
+            
+            if(!ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _usuarioService.AddUsuario(usuario);
+                return CreatedAtRoute("getUsuario", new{id = usuario.id}, usuario);
+            }
+        }
+
+        [HttpDelete("{id")]
+        public ActionResult Delete(int id){
+            if(id == 0 || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _usuarioService.delete(id);
+                return Ok();
+            }
+        }
+
+        [HttpPut("id")]
+        public ActionResult Update(int id, [FromBody] Usuarios usuario){
+            if(usuario.id != id || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _usuarioService.update(id, usuario);
+                return Ok();
+            }
+        }
+
     }
 }

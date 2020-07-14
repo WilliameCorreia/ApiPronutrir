3152f5e91ff3e35132400804ffbfc7d34518fd6c
williame <williame_lima@hotmail.com>
2020-07-14 14:47:55 -0300

    controllerMedicamentos ContollerCategorias

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,7 +1,69 @@
+using ApiPronutrir.Models;
+using ApiPronutrir.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ApiPronutrir.Controllers
 {
-    public class CategoriaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
     {
-        
+        private readonly CategoriaService _categoriaService;
+
+        public CategoriaController(CategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll(){
+
+            var Lista_categorias = _categoriaService.GetAllCategorias();
+
+            return Ok(Lista_categorias);
+        }
+
+        [HttpGet("{id}", Name="getCategoria")]
+        public ActionResult GetById(int id){
+
+            var categoria = _categoriaService.find(id);
+
+            if(!ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                return new ObjectResult(categoria);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] Categorias categoria){
+            
+            if(!ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _categoriaService.AddCategoria(categoria);
+                return CreatedAtRoute("getCategoria", new{id = categoria.id}, categoria);
+            }
+        }
+
+        [HttpDelete("{id")]
+        public ActionResult Delete(int id){
+            if(id == 0 || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _categoriaService.delete(id);
+                return Ok();
+            }
+        }
+
+        [HttpPut("id")]
+        public ActionResult Update(int id, [FromBody] Categorias categoria){
+            if(categoria.id != id || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _categoriaService.update(id, categoria);
+                return Ok();
+            }
+        }
     }
 }
--- Controllers/MedicamentoController.cs
+++ Controllers/MedicamentoController.cs
@@ -1,7 +1,77 @@
+using ApiPronutrir.Models;
+using ApiPronutrir.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ApiPronutrir.Controllers
 {
-    public class MedicamentoController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicamentoController : ControllerBase
     {
-        
+        private readonly MedicamentoService _medicamentoService;
+
+        public MedicamentoController(MedicamentoService medicamentoService)
+        {
+            _medicamentoService = medicamentoService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+
+            var Lista_medicamentos = _medicamentoService.GetAllMedicamentos();
+
+            return Ok(Lista_medicamentos);
+
+        }
+
+        [HttpGet("{id", Name = "getMedicamento")]
+        public ActionResult GetById(int id)
+        {
+
+            var medicamento = _medicamentoService.find(id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                return new ObjectResult(medicamento);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Create([FromBody] Medicamentos medicamento){
+            if(!ModelState.IsValid){
+               return BadRequest(); 
+            }else{
+                _medicamentoService.AddMedicamento(medicamento);
+                return CreatedAtRoute("getMedicamento", new{id = medicamento.id}, medicamento);
+            }
+        }
+
+        [HttpDelete("{id")]
+        public ActionResult Delete(int id){
+            if(id == 0 || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _medicamentoService.delete(id);
+                return Ok();
+            }
+        }
+
+        [HttpPut("id")]
+        public ActionResult Update(int id, [FromBody] Medicamentos medicamento){
+            if(medicamento.id != id || !ModelState.IsValid){
+                return BadRequest(ModelState);
+            }else{
+                _medicamentoService.update(id, medicamento);
+                return Ok();
+            }
+        }
+
+
     }
 }
